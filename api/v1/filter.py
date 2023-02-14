4135ccd55790055b466c24ff98d4efcfa2b0b8c9
Aspect13 <ad13box@gmail.com>
2023-02-14 15:35:59 +0400

    making filter_manager

--- api/v1/filter.py
+++ api/v1/filter.py
@@ -1,4 +1,3 @@
-import json
 from datetime import datetime
 from queue import Empty
 from typing import Optional
@@ -9,9 +8,10 @@ from collections import defaultdict
 
 from pylon.core.tools import log
 
-from ...utils import process_query_result, upload_to_minio, merge_comparisons, ComparisonDataStruct
+from ...utils import process_query_result, merge_comparisons, FilterManager
+from ...models.pd.builder_data import ComparisonDataStruct
 
-from tools import MinioClient
+# from tools import MinioClient
 
 
 class API(Resource):
@@ -119,27 +119,35 @@ class API(Resource):
         # if we add tests to existing comparison data we do not want
         # to recalculate all comparison data. We use existing one and merge with new
         merge_source_hash: Optional[str] = request.json.get('merge_with_source')
+        filter_manager = FilterManager(
+            project,
+            self.module.descriptor.config.get('bucket_name', 'comparison'),
+        )
         if merge_source_hash:
-            source_data = MinioClient(project).download_file(
-                self.module.descriptor.config.get('bucket_name', 'comparison'),
-                f'{merge_source_hash}.json'
+            filter_manager.source_hash = merge_source_hash
+            source_data = filter_manager.get_filters(f'{filter_manager.source_hash}.json')
+            filter_manager.client.download_file(
+                filter_manager.bucket_name,
+                f'{filter_manager.source_hash}'
             )
-            source_data = source_data.decode('utf-8')
-            source_data = json.loads(source_data)
+            # source_data = MinioClient(project).download_file(
+            #     self.module.descriptor.config.get('bucket_name', 'comparison'),
+            #     f'{merge_source_hash}.json'
+            # )
+            # source_data = source_data.decode('utf-8')
+            # source_data = json.loads(source_data)
             data = merge_comparisons(source_data, data)
         else:
             # Normalize output
             data = ComparisonDataStruct.parse_obj(data)
 
-        hash_name = upload_to_minio(
-            project,
+        filter_manager.source_hash = filter_manager.upload_to_minio(
             data=data.json(
                 exclude_none=True, exclude_defaults=True,
                 by_alias=True, sort_keys=True, ensure_ascii=False
             ).encode('utf-8'),
-            bucket_name=self.module.descriptor.config.get('bucket_name', 'comparison')
         )
-        hash_name = hash_name[:-len('.json')]
+        hash_name = filter_manager.source_hash[:-len('.json')]
 
         url_base = url_for('theme.index', _external=True, _scheme=request.headers.get('X-Forwarded-Proto', 'http'))
         return redirect(f'{url_base}-/performance/analysis/compare?source={hash_name}')
--- utils.py
+++ utils.py
@@ -25,23 +25,23 @@ def process_query_result(plugin, query_data) -> Iterable[Union[BackendAnalysisMo
     )
 
 
-def upload_to_minio(project, data: bytes,
-                    file_name: Optional[str] = None,
-                    bucket_name: str = 'comparison'
-                    ):
-    file = BytesIO()
-    file.write(data)
-    file.seek(0)
-    # log.debug('File created')
-    client = MinioClient(project=project)
-    if bucket_name not in client.list_bucket():
-        client.create_bucket(bucket_name)
-        client.configure_bucket_lifecycle(bucket_name, days=7)
-    if not file_name:
-        file_name = f'{md5(file.getbuffer()).hexdigest()}.json'
-    # log.debug('Bucket created [%s]', bucket_name)
-    client.upload_file(bucket_name, file, file_name)
-    return file_name
+# def upload_to_minio(project, data: bytes,
+#                     file_name: Optional[str] = None,
+#                     bucket_name: str = 'comparison'
+#                     ):
+#     file = BytesIO()
+#     file.write(data)
+#     file.seek(0)
+#     # log.debug('File created')
+#     client = MinioClient(project=project)
+#     if bucket_name not in client.list_bucket():
+#         client.create_bucket(bucket_name)
+#         client.configure_bucket_lifecycle(bucket_name, days=7)
+#     if not file_name:
+#         file_name = f'{md5(file.getbuffer()).hexdigest()}.json'
+#     # log.debug('Bucket created [%s]', bucket_name)
+#     client.upload_file(bucket_name, file, file_name)
+#     return file_name
 
 
 def merge_comparisons(source_data: dict, current_data: dict,
@@ -57,28 +57,27 @@ def merge_comparisons(source_data: dict, current_data: dict,
     )
 
 
-def get_minio_file_data_or_none(project, bucket_name: str, file_name: str) -> Optional[str]:
-    # if not file_name:
-    #     return
-    # log.info('get start %s', [bucket_name, file_name])
-    try:
-        file_data = MinioClient(project).download_file(bucket_name, file_name)
-    except Exception as e:
-        # log.info('get_minio_file_data_or_none %s', e)
-        return
-    return file_data.decode('utf-8')
+# def get_minio_file_data_or_none(project, bucket_name: str, file_name: str) -> Optional[str]:
+#     # if not file_name:
+#     #     return
+#     # log.info('get start %s', [bucket_name, file_name])
+#     try:
+#         file_data = MinioClient(project).download_file(bucket_name, file_name)
+#     except Exception as e:
+#         # log.info('get_minio_file_data_or_none %s', e)
+#         return
+#     return file_data.decode('utf-8')
 
 
-def get_persistent_filters_file_name(source_hash: str, user_id: int) -> str:
-    return f'{source_hash}_user_filters_{user_id}.json'
+# def get_persistent_filters_file_name(source_hash: str, user_id: int) -> str:
+#     return f'{source_hash}_user_filters_{user_id}.json'
 
 
-def get_persistent_filters(project, bucket_name: str, source_hash: str, user_id: int) -> List[dict]:
-    file_name = get_persistent_filters_file_name(source_hash, user_id)
-    filters = get_minio_file_data_or_none(project, bucket_name=bucket_name, file_name=file_name)
-    if not filters:
-        return []
-    return json.loads(filters)
+# def get_filters(project, bucket_name: str, filter_file_name: str) -> List[dict]:
+#     filters = get_minio_file_data_or_none(project, bucket_name=bucket_name, file_name=filter_file_name)
+#     if not filters:
+#         return []
+#     return json.loads(filters)
     # return [{
     #     "id": 1675947643488,
     #     "type": "ui_performance",
@@ -89,3 +88,69 @@ def get_persistent_filters(project, bucket_name: str, source_hash: str, user_id:
     #         "lvc"
     #     ]
     # }]
+
+# def get_persistent_filters(project, bucket_name: str, source_hash: str, user_id: int) -> List[dict]:
+#     file_name = get_persistent_filters_file_name(source_hash, user_id)
+#     return get_filters(project, bucket_name, )
+
+class FilterManager:
+    RETENTION_DAYS: int = 7
+
+    def __init__(self, project, bucket_name: str, source_hash: Optional[str] = None):
+        self.project = project
+        self.bucket_name = bucket_name
+        self._source_hash = source_hash
+        self._client = None
+        self.create_bucket()
+
+    @property
+    def source_hash(self) -> Optional[str]:
+        if not self._source_hash:
+            raise NotImplementedError('Variable source_hash in None')
+        return self._source_hash
+
+    @source_hash.setter
+    def source_hash(self, value: str):
+        self._source_hash = value
+
+    @property
+    def client(self):
+        if not self._client:
+            self._client = MinioClient(self.project)
+        return self._client
+
+    def create_bucket(self) -> None:
+        if self.bucket_name not in self.client.list_bucket():
+            self.client.create_bucket(self.bucket_name)
+            self.client.configure_bucket_lifecycle(self.bucket_name, days=self.RETENTION_DAYS)
+
+    def get_minio_file_data_or_none(self, file_name: str) -> Optional[str]:
+        try:
+            file_data = self.client.download_file(self.bucket_name, file_name)
+        except Exception as e:
+            # log.info('get_minio_file_data_or_none %s', e)
+            return
+        return file_data.decode('utf-8')
+
+    def get_filters(self, filter_file_name: str, default=[]):
+        filters = self.get_minio_file_data_or_none(filter_file_name)
+        try:
+            return json.loads(filters)
+        except TypeError:
+            return default
+
+    def get_user_filters_name(self, user_id: int) -> str:
+        return f'{self.source_hash}_user_filters_{user_id}.json'
+
+    def get_user_filters(self, user_id: int) -> List[dict]:
+        file_name = self.get_user_filters_name(user_id)
+        return self.get_filters(file_name, default=[])
+
+    def upload_to_minio(self, data: bytes, file_name: Optional[str] = None) -> str:
+        file = BytesIO()
+        file.write(data)
+        file.seek(0)
+        if not file_name:
+            file_name = f'{md5(file.getbuffer()).hexdigest()}.json'
+        self.client.upload_file(self.bucket_name, file, file_name)
+        return file_name
